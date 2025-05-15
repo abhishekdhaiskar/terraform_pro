a67788b303ab67b7b5daf4a468b4c862b70db969
abhi <abhishekdhiskar2003@gmail.com>
2025-05-15 19:25:06 +0530

    Updated AMI ID to valid value

--- Day-1/main.tf
+++ Day-1/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "name" {
-    ami = "ami-0c02fb55956c7d316"  # Amazon Linux 2 AMI (us-east-1)
+    ami = "ami-04999cd8f2624f834"  # Amazon Linux 2 AMI (us-east-1)
   instance_type = "t2.micro"
 
   tags = {
